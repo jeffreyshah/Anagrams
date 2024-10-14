0b440763c0ea7a0fdb3bb7296445d699d360f950
Ariel Montero <timelord1102@gmail.com>
2024-10-14 16:09:21 -0400

    sets up React and Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>SCRAMBLED!</title>
+        <script src="" defer></script>
+      </head>
+      <body>
+        <div className="title">SCRAMBLED!</div>
+        <div className="description"></div>
+        <button id="singleplayer">Single Player</button>
+        <button id="multiplayer">Play with Friends</button>
+      </body>
+    </html>
+  );
+}
