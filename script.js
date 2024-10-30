0ae33f91c348330f7839ad58de07520fbb51aa9f
duhrice <ivan.chen223@gmail.com>
2024-10-29 22:26:37 -0400

    Styled landing page and added temporary scripted

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("play-button").addEventListener("click", function () {
+  alert("Play button clicked");
+});
+
+document.getElementById("join-button").addEventListener("click", function () {
+  alert("Join Game clicked");
+});
