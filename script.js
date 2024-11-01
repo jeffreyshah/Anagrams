2ebe1bb57575a75ffc86b3db19d9400e45b6dcb1
duhrice <ivan.chen223@gmail.com>
2024-11-01 14:05:13 -0400

    Slight changes to script

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Home page
 document.getElementById("play-button").addEventListener("click", function () {
   alert("Play button clicked");
 });
 
 document.getElementById("join-button").addEventListener("click", function () {
-  alert("Join Game clicked");
+  alert("Multiplayer will be implemented in the future.");
 });
