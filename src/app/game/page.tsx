939adc4c3d4c4c263f7149438b717d70381489f2
duhrice <ivan.chen223@gmail.com>
2024-11-05 13:52:57 -0500

    Stylized game page

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,93 +1,106 @@
-'use client'
+"use client";
 
-import React, { useState, useEffect, useRef } from 'react';
-import { getWord, checkWord } from '../server/game';
-import "../style.css" // Adjust the path as necessary
-import Link from 'next/link';
+import React, { useState, useEffect, useRef } from "react";
+import { getWord, checkWord } from "../server/game";
+import "../style.css"; // Adjust the path as necessary
+import Link from "next/link";
 
 const GamePage: React.FC = () => {
-    const [letters, setLetters] = useState<string[]>([]);
-    const [word, setWord] = useState<string>('');
-    const [isWordValid, setIsWordValid] = useState<boolean | null>(null);
-    const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
+  const [letters, setLetters] = useState<string[]>([]);
+  const [word, setWord] = useState<string>("");
+  const [isWordValid, setIsWordValid] = useState<boolean | null>(null);
+  const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
 
-    useEffect(() => {
-        const fetchWord = async () => {
-            try {
-                const fetchedWord = await getWord();
-                console.log('Fetched word:', fetchedWord); // Log the fetched word
-                setWord(fetchedWord);
-                setLetters(Array(fetchedWord.length).fill(''));
-            } catch (error) {
-                console.error('Error fetching word:', error);
-            }
-        };
-        fetchWord();
-    }, []);
-
-    const handleChange = (index: number, value: string) => {
-        if (value.length <= 1) {
-            const newLetters = [...letters];
-            newLetters[index] = value;
-            setLetters(newLetters);
-
-            // Move to the next input field if it exists
-            if (value && index < inputRefs.current.length - 1) {
-                inputRefs.current[index + 1]?.focus();
-            }
-        }
+  useEffect(() => {
+    const fetchWord = async () => {
+      try {
+        const fetchedWord = await getWord();
+        console.log("Fetched word:", fetchedWord); // Log the fetched word
+        setWord(fetchedWord);
+        setLetters(Array(fetchedWord.length).fill(""));
+      } catch (error) {
+        console.error("Error fetching word:", error);
+      }
     };
+    fetchWord();
+  }, []);
 
-    const handleKeyPress = async (event: React.KeyboardEvent<HTMLInputElement>, index: number) => {
-        if (event.key === 'Enter') {
-            const formedWord = letters.join('');
-            const isValid = await checkWord(formedWord, word);
-            console.log('Is ' + word + ' valid:', isValid); // Log if the word is valid
-            setIsWordValid(isValid);
+  const handleChange = (index: number, value: string) => {
+    if (value.length <= 1) {
+      const newLetters = [...letters];
+      newLetters[index] = value;
+      setLetters(newLetters);
 
-            if (!isValid) {
-                // Clear the textboxes and reset cursor position
-                setLetters(Array(word.length).fill(''));
-                inputRefs.current[0]?.focus();
-            }
-        } else if (event.key === 'Backspace' && !letters[index]) {
-            if (index > 0) {
-                inputRefs.current[index - 1]?.focus();
-            }
-        }
-    };
+      // Move to the next input field if it exists
+      if (value && index < inputRefs.current.length - 1) {
+        inputRefs.current[index + 1]?.focus();
+      }
+    }
+  };
 
-    const handleFirstInputFocus = () => {
-        setLetters(Array(word.length).fill(''));
-    };
+  const handleKeyPress = async (
+    event: React.KeyboardEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    if (event.key === "Enter") {
+      const formedWord = letters.join("");
+      const isValid = await checkWord(formedWord, word);
+      console.log("Is " + word + " valid:", isValid); // Log if the word is valid
+      setIsWordValid(isValid);
 
-    return (
-        <div>
-            <h1>Game Page</h1>
-            <div>
-                <h2>{word}</h2>
-                {letters.map((letter, index) => (
-                    <input
-                        key={index}
-                        type="text"
-                        value={letter}
-                        onChange={(e) => handleChange(index, e.target.value)}
-                        onKeyDown={(e) => handleKeyPress(e, index)}
-                        maxLength={1}
-                        style={{ width: '20px', marginRight: '5px' }}
-                        ref={(el) => (inputRefs.current[index] = el)}
-                        onFocus={index === 0 ? handleFirstInputFocus : undefined}
-                    />
-                ))}
-            </div>
-            {isWordValid !== null && (
-                <div className='game-over-div'>
-                      {isWordValid ? 'The word is valid!' : 'The word is not valid.'}
-                      {isWordValid ? <Link href="/game" className='game-button'onClick={() => window.location.reload()}>Play again</Link> : null}
-                </div>
-            )}
+      if (!isValid) {
+        // Clear the textboxes and reset cursor position
+        setLetters(Array(word.length).fill(""));
+        inputRefs.current[0]?.focus();
+      }
+    } else if (event.key === "Backspace" && !letters[index]) {
+      if (index > 0) {
+        inputRefs.current[index - 1]?.focus();
+      }
+    }
+  };
+
+  const handleFirstInputFocus = () => {
+    setLetters(Array(word.length).fill(""));
+  };
+
+  return (
+    <body className="game-page">
+      <h1 className="game-title">
+        SCRAMB<span className="tilted-letter">L</span>ED
+      </h1>
+      <div className="game-container">
+        <h2 className="game-word">{word}</h2>
+        {letters.map((letter, index) => (
+          <input
+            className="game-input"
+            key={index}
+            type="text"
+            value={letter}
+            onChange={(e) => handleChange(index, e.target.value)}
+            onKeyDown={(e) => handleKeyPress(e, index)}
+            maxLength={1}
+            ref={(el) => (inputRefs.current[index] = el)}
+            onFocus={index === 0 ? handleFirstInputFocus : undefined}
+          />
+        ))}
+      </div>
+      {isWordValid !== null && (
+        <div className="game-over-div">
+          {isWordValid ? "The word is valid!" : "The word is not valid."}
+          {isWordValid ? (
+            <Link
+              href="/game"
+              className="game-button"
+              onClick={() => window.location.reload()}
+            >
+              Play again
+            </Link>
+          ) : null}
         </div>
-    );
+      )}
+    </body>
+  );
 };
 
 export default GamePage;
--- src/app/pages/gamePage.tsx
+++ src/app/pages/gamePage.tsx
@@ -1,71 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import { getWord, checkWord } from '../server/game'; // Adjust the path as necessary
-import "../../style.css";
+import React, { useState, useEffect } from "react";
+import { getWord, checkWord } from "../server/game"; // Adjust the path as necessary
+import "../style.css";
 
 const GamePage: React.FC = () => {
-    const [letters, setLetters] = useState<string[]>(Array(5).fill(''));
-    const [word, setWord] = useState<string>('');
-    const [isWordValid, setIsWordValid] = useState<boolean | null>(null);
+  const [letters, setLetters] = useState<string[]>(Array(5).fill(""));
+  const [word, setWord] = useState<string>("");
+  const [isWordValid, setIsWordValid] = useState<boolean | null>(null);
 
-    useEffect(() => {
-        const fetchWord = async () => {
-            const fetchedWord = await getWord();
-            setWord(fetchedWord);
-        };
-        fetchWord();
-    }, []);
-
-    const handleChange = (index: number, value: string) => {
-        if (value.length <= 1) {
-            const newLetters = [...letters];
-            newLetters[index] = value;
-            setLetters(newLetters);
-        }
+  useEffect(() => {
+    const fetchWord = async () => {
+      const fetchedWord = await getWord();
+      setWord(fetchedWord);
     };
+    fetchWord();
+  }, []);
 
-    const handleKeyPress = async (event: React.KeyboardEvent<HTMLInputElement>) => {
-        if (event.key === 'Enter') {
-            const formedWord = letters.join('');
-            const isValid = await checkWord(formedWord);
-            setIsWordValid(isValid);
-        }
-    };
+  const handleChange = (index: number, value: string) => {
+    if (value.length <= 1) {
+      const newLetters = [...letters];
+      newLetters[index] = value;
+      setLetters(newLetters);
+    }
+  };
 
-    return (
-        <body className="game-page">
-            <h1 className="game-title">SCRAMB<span className="tilted-letter">L</span>ED</h1>
-            <div className="game-container">
-                <h2 className="game-word">{word}</h2>
-                {letters.map((letter, index) => (
-                    <input className="game-input"
-                        key={index}
-                        type="text"
-                        value={letter}
-                        onChange={(e) => handleChange(index, e.target.value)}
-                        onKeyDown={handleKeyPress}
-                        maxLength={1}
-                        style={{
-                            height: '100px',
-                            width: '90px',
-                            marginRight: '20px',
-                            textAlign: 'center',
-                            fontFamily: 'Habibi, serif',
-                            fontSize: '60px',
-                            color: '#000000',
-                            backgroundColor: '#e5e1d4',
-                            border: '2px solid #000000',
-                            borderRadius: '5px'
-                        }}
-                    />
-                ))}
-            </div>
-            {isWordValid !== null && (
-                <div>
-                    {isWordValid ? 'The word is valid!' : 'The word is not valid.'}
-                </div>
-            )}
-        </body>
-    );
+  const handleKeyPress = async (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ) => {
+    if (event.key === "Enter") {
+      const formedWord = letters.join("");
+      const isValid = await checkWord(formedWord);
+      setIsWordValid(isValid);
+    }
+  };
+
+  return (
+    <body className="game-page">
+      <h1 className="game-title">
+        SCRAMB<span className="tilted-letter">L</span>ED
+      </h1>
+      <div className="game-container">
+        <h2 className="game-word">{word}</h2>
+        {letters.map((letter, index) => (
+          <input
+            className="game-input"
+            key={index}
+            type="text"
+            value={letter}
+            onChange={(e) => handleChange(index, e.target.value)}
+            onKeyDown={handleKeyPress}
+            maxLength={1}
+          />
+        ))}
+      </div>
+      {isWordValid !== null && (
+        <div>
+          {isWordValid ? "The word is valid!" : "The word is not valid."}
+        </div>
+      )}
+    </body>
+  );
 };
 
 export default GamePage;
