a8ccf455ed38de247dfe06067103ef176969155e
Ariel Montero <timelord1102@gmail.com>
2024-11-22 15:23:55 -0500

    adds word list  and better stats page

--- src/app/singleplayer/page.tsx
+++ src/app/singleplayer/page.tsx
@@ -11,10 +11,10 @@ The Single PLayer game page
 
 This page contains the logic for the single player game mode. 
 The player...
-    is given a scrambled word and has to unscramble it by typing the correct word. 
-    can submit words by typing them in the input boxes and pressing enter.
-    can also delete the previous letter by pressing backspace. 
-    earns points for each valid word submitted, the points are based on the length of the word.
+  is given a scrambled word and has to unscramble it by typing the correct word. 
+  can submit words by typing them in the input boxes and pressing enter.
+  can also delete the previous letter by pressing backspace. 
+  earns points for each valid word submitted, the points are based on the length of the word.
 The game ends after 60 seconds. 
 The player can play again after the game ends.
 
@@ -34,198 +34,219 @@ const Singleplayer: React.FC = () => {
   const inputRefs = useRef<(HTMLInputElement | null)[]>([]);
   const timerRef = useRef<NodeJS.Timeout | null>(null);
 
+  const foundWords = new Set<string>();
+
   // Resets game to initial state (triggered on on play again)
   const resetGame = async () => {
-    setInputLetters(Array(6).fill(""));
-    setCurrentIndex(0);
-    setValidWords(new Set());
-    setScore(0);
-    setTimeLeft(ROUND_TIME_LIMIT);
-    setIsGameOver(false);
+  setInputLetters(Array(6).fill(""));
+  setCurrentIndex(0);
+  setValidWords(new Set());
+  setScore(0);
+  setTimeLeft(ROUND_TIME_LIMIT);
+  setIsGameOver(false);
 
-    try {
-      const fetchedWord = await getSingleplayerWord();
-      setScrambledWord(fetchedWord);
-    } catch (error) {
-      console.error("Error fetching word:", error);
+  try {
+    const fetchedWord = await getSingleplayerWord();
+    setScrambledWord(fetchedWord);
+  } catch (error) {
+    console.error("Error fetching word:", error);
+  }
+
+  if (timerRef.current) clearInterval(timerRef.current);
+  timerRef.current = setInterval(() => {
+    setTimeLeft((prevTime) => {
+    const newTime = Math.max(prevTime - 1, 0);
+    if (newTime === 0) {
+      clearInterval(timerRef.current!);
+      setIsGameOver(true);
     }
+    return newTime;
+    });
+  }, 1000);
 
-    if (timerRef.current) clearInterval(timerRef.current);
-    timerRef.current = setInterval(() => {
-      setTimeLeft((prevTime) => {
-        const newTime = Math.max(prevTime - 1, 0);
-        if (newTime === 0) {
-          clearInterval(timerRef.current!);
-          setIsGameOver(true);
-        }
-        return newTime;
-      });
-    }, 1000);
-
-    setTimeout(() => {
-      inputRefs.current[0]?.focus();
-    }, 0);
+  setTimeout(() => {
+    inputRefs.current[0]?.focus();
+  }, 0);
   };
 
   useEffect(() => {
-    const fetchWord = async () => {
-      try {
-        const fetchedWord = await getSingleplayerWord();
-        setScrambledWord(fetchedWord);
-      } catch (error) {
-        console.error("Error fetching word:", error);
-      }
-    };
-    fetchWord();
+  const fetchWord = async () => {
+    try {
+    const fetchedWord = await getSingleplayerWord();
+    setScrambledWord(fetchedWord);
+    } catch (error) {
+    console.error("Error fetching word:", error);
+    }
+  };
+  fetchWord();
 
-    timerRef.current = setInterval(() => {
-      setTimeLeft((prevTime) => {
-        const newTime = Math.max(prevTime - 1, 0);
-        if (newTime === 0) {
-          clearInterval(timerRef.current!);
-          setIsGameOver(true);
-        }
-        return newTime;
-      });
-    }, 1000);
+  timerRef.current = setInterval(() => {
+    setTimeLeft((prevTime) => {
+    const newTime = Math.max(prevTime - 1, 0);
+    if (newTime === 0) {
+      clearInterval(timerRef.current!);
+      setIsGameOver(true);
+    }
+    return newTime;
+    });
+  }, 1000);
 
-    return () => {
-      if (timerRef.current) clearInterval(timerRef.current);
-    };
+  return () => {
+    if (timerRef.current) clearInterval(timerRef.current);
+  };
   }, []);
 
   // Check if the word is valid and update score. Trigger shake animation on invalid word
   const handleSubmitWord = async () => {
-    const word = inputLetters.join("").trim();
-    if (word.length >= 3 && word.length <= 6 && !validWords.has(word)) {
-      const isValid = await checkAnyWord(word, scrambledWord);
-      if (isValid) {
+  const word = inputLetters.join("").trim();
+  if (word.length >= 3 && word.length <= 6 && !validWords.has(word)) {
+    const isValid = await checkAnyWord(word, scrambledWord);
+    if (isValid) {
 
-        const audioFile = word.length === 6 ? "/sounds/reward.mp3" : "/sounds/newArtifact.mp3";
-        const audio = new Audio(audioFile);
-        audio.play().catch((error) => console.error("Error playing sound:", error));
+    const audioFile = word.length === 6 ? "/sounds/reward.mp3" : "/sounds/newArtifact.mp3";
+    const audio = new Audio(audioFile);
+    audio.play().catch((error) => console.error("Error playing sound:", error));
 
-        setValidWords(new Set(validWords.add(word)));
-        const scoreMapping: Record<number, number> = {
-          3: 100,
-          4: 400,
-          5: 1200,
-          6: 2000,
-        };
-        const points = scoreMapping[word.length] || 0;
-        setScore((prevScore) => prevScore + points);
-      } else {
-        triggerShake(); // Trigger shake animation on invalid word
-      }
+    setValidWords(new Set(validWords.add(word)));
+    const scoreMapping: Record<number, number> = {
+      3: 100,
+      4: 400,
+      5: 1200,
+      6: 2000,
+    };
+    const points = scoreMapping[word.length] || 0;
+    setScore((prevScore) => prevScore + points);
+    foundWords.add(word);
+ // Update the validWords state to trigger re-render
     } else {
-      triggerShake(); // Trigger shake animation for duplicate or invalid length
+    triggerShake(); // Trigger shake animation on invalid word
     }
-    setInputLetters(Array(6).fill(""));
-    setCurrentIndex(0);
-    inputRefs.current[0]?.focus();
+  } else {
+    triggerShake(); // Trigger shake animation for duplicate or invalid length
+  }
+  setInputLetters(Array(6).fill(""));
+  setCurrentIndex(0);
+  inputRefs.current[0]?.focus();
   };
 
   const triggerShake = () => {
-    setShake(true);
-    setTimeout(() => {
-      setShake(false); 
-    }, 500); 
+  setShake(true);
+  setTimeout(() => {
+    setShake(false); 
+  }, 500); 
   };
 
   // Handle backspace and enter key press (removes the previous letter on backspace, submits the word on enter)
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, index: number) => {
-    if (e.key === "Backspace" && !isGameOver) {
-      const updatedLetters = [...inputLetters];
-      updatedLetters[index] = "";
-      if (index > 0) {
-        updatedLetters[index - 1] = "";
-        setCurrentIndex(index - 1);
-        inputRefs.current[index - 1]?.focus();
-      }
-      setInputLetters(updatedLetters);
-    } else if (e.key === "Enter" && !isGameOver) {
-      handleSubmitWord();
+  if (e.key === "Backspace" && !isGameOver) {
+    const updatedLetters = [...inputLetters];
+    updatedLetters[index] = "";
+    if (index > 0) {
+    updatedLetters[index - 1] = "";
+    setCurrentIndex(index - 1);
+    inputRefs.current[index - 1]?.focus();
     }
+    setInputLetters(updatedLetters);
+  } else if (e.key === "Enter" && !isGameOver) {
+    handleSubmitWord();
+  }
   };
 
   // Play sound on game over
   useEffect(() => {
-    if(isGameOver) {
-        if(score < 1000) {
-            const audio= new Audio("/sounds/hellnaw.mp3");
-            audio.play().catch((error) => console.error("Error playing audio:", error));
-        }
-      
+  if(isGameOver) {
+    if(score < 1000) {
+      const audio= new Audio("/sounds/hellnaw.mp3");
+      audio.play().catch((error) => console.error("Error playing audio:", error));
     }
+    
+  }
   }, [isGameOver, score]);
 
   // Handle input change and move to next input field
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
-    if (!isGameOver) {
-      const value = e.target.value.toLowerCase();
-      if (value.length === 1 && /^[a-z]$/.test(value)) {
-        const updatedLetters = [...inputLetters];
-        updatedLetters[index] = value;
-        setInputLetters(updatedLetters);
+  if (!isGameOver) {
+    const value = e.target.value.toLowerCase();
+    if (value.length === 1 && /^[a-z]$/.test(value)) {
+    const updatedLetters = [...inputLetters];
+    updatedLetters[index] = value;
+    setInputLetters(updatedLetters);
 
-        if (index < 5 && updatedLetters[index + 1] === "") {
-          setCurrentIndex(index + 1);
-          inputRefs.current[index + 1]?.focus();
-        }
-      }
+    if (index < 5 && updatedLetters[index + 1] === "") {
+      setCurrentIndex(index + 1);
+      inputRefs.current[index + 1]?.focus();
     }
+    }
+  }
   };
 
   const formattedScore = score.toString().padStart(4, "0");
 
   return (
-    <div className="game-container">
-      <button className="home-button" onClick={() => window.location.href = '/'}> 
-        <i className="fas fa-home"></i> 
-      </button>
-      <h1 className="game-title">
-        SCRAMB<span className="tilted-letter">L</span>ED
-      </h1>
-      <div className={`game-content ${shake ? "shake" : ""}`}>
-        <h2 className="game-word">{scrambledWord}</h2>
-        <div className="input-boxes">
-          {inputLetters.map((letter, index) => (
-            <input
-              key={index}
-              type="text"
-              value={letter}
-              maxLength={1}
-              ref={(el) => {
-                inputRefs.current[index] = el;
-              }}
-              onChange={(e) => handleInputChange(e, index)}
-              onKeyDown={(e) => handleKeyDown(e, index)}
-              disabled={isGameOver}
-              className={`game-input ${shake ? "shake" : ""}`} // Add the shake class here
-              autoFocus={index === currentIndex}
-            />
-          ))}
-        </div>
+  <div className="singleplayer-container">
+    <h1 className="game-title">
+    SCRAMB<span className="tilted-letter">L</span>ED
+    </h1>
+  <div className="game-container-container">
+  <div className="game-container">
+    <button className="home-button" onClick={() => window.location.href = '/'}> 
+    <i className="fas fa-home"></i> 
+    </button>
+    {!isGameOver && (
+    <div className={`game-content ${shake ? "shake" : ""}`}>
+      <h2 className="game-word">{scrambledWord}</h2>
+      <div className="input-boxes">
+      {inputLetters.map((letter, index) => (
+        <input
+        key={index}
+        type="text"
+        value={letter}
+        maxLength={1}
+        ref={(el) => {
+          inputRefs.current[index] = el;
+        }}
+        onChange={(e) => handleInputChange(e, index)}
+        onKeyDown={(e) => handleKeyDown(e, index)}
+        disabled={isGameOver}
+        className={`game-input ${shake ? "shake" : ""}`} // Add the shake class here
+        autoFocus={index === currentIndex}
+        />
+      ))}
       </div>
-      <div className="stats-container">
-          <div className="stats-icon">
-            <img src="/images/lebron.webp" alt="Stats Icon" className="icon-image" />
-          </div>
-          <div className="stats-text">
-            <div className="stats-words">WORDS: {validWords.size} </div>
-            <div className="stats-score">SCORE: {formattedScore}</div>
-          </div>
-        </div>
-      <div className="timer">Time Left: {timeLeft}s</div>
-      {isGameOver && (
-        <div className="game-over">
-          Game Over!
-          <button className="game-button" onClick={resetGame}>
-            Play Again
-          </button>
-        </div>
-      )}
     </div>
+    )}
+    <div className="stats-container">
+      <div className="stats-icon">
+      <Image src="/images/lebron.webp" alt="Stats Icon" className="icon-image"  width={24} height={24}/>
+      </div>
+      <div className="stats-text">
+      <div className="stats-words">WORDS: {validWords.size} </div>
+      <div className="stats-score">SCORE: {formattedScore}</div>
+      </div>
+      {isGameOver && (
+        <div className="word-container">
+        <div className="word-list">
+          <h3>Words Found:</h3>
+          <div>
+            {Array.from(validWords).map((word, index) => (
+          <li key={index} style={{ listStyleType: "none" }}>{word}</li>
+            ))}
+          </div>
+          </div>
+          </div>)}
+    </div>
+    {!isGameOver && <div className="timer">Time Left: {timeLeft}s</div>}
+    {isGameOver && (
+    <div className="game-over">
+      Game Over!
+      <button className="game-button" onClick={resetGame}>
+      Play Again
+      </button>
+    </div>
+    )}
+  </div>
+  </div>
+  </div>
   );
 };
 
